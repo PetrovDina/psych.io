1c89dcc71b6394109444518ab4e90fa6113c3702
stasadj <anastasijadjuric@uns.ac.rs>
2022-05-22 22:35:59 +0200

    init

---
+++ psychio/src/main/java/com/sbnz/psychio/PsychioApplication.java
@@ -0,0 +1,13 @@
+package com.sbnz.psychio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychioApplication.class, args);
+	}
+
+}
---
+++ psychio/src/main/java/com/sbnz/psychio/config/KieConfiguration.java
@@ -0,0 +1,21 @@
+package com.sbnz.psychio.config;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KieConfiguration {
+
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("com.sbnz", "psychio-kjar", "0.0.1-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        kScanner.start(10_000);
+        return kContainer;
+    }
+
+}

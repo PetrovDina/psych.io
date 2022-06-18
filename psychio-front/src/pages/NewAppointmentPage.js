798bd9e1154650811f12b477fe1f993ae62844f8
Dina Petrov <dina.petrov@gmail.com>
2022-06-18 15:41:01 +0200

    Showing statements after symptom submit

--- psychio-front/src/pages/NewAppointmentPage.js
+++ psychio-front/src/pages/NewAppointmentPage.js
@@ -12,6 +12,12 @@ const NewAppointmentPage = () => {
 
     const frequencies = ["NEVER", "SOMETIMES", "OFTEN"]
 
+    const [statementsLoaded, setStatementsLoaded] = useState(false);
+    const [statements, setStatements] = useState([]);
+
+    const statementResponses = ["STRONGLY_DISAGREE", "DISAGREE", "NA", "AGREE", "STRONGLY_AGREE"]
+
+
     useEffect(() => {
         SymptomService.getAll()
             .then((response) => {
@@ -39,63 +45,125 @@ const NewAppointmentPage = () => {
 
         SymptomService.submitSymptomFrequencies(object)
             .then((response) => {
+                console.log(response);
+                setStatements(response.statements);
+                setStatementsLoaded(true);
 
             })
+    }
 
+    function onStatementResponseChange(statementIndex, response) {
+        let updatedStatements = [...statements];
+        updatedStatements[statementIndex].response = response;
+        setStatements(updatedStatements);
 
     }
 
+    function submitStatements() {
+
+        console.log(statements);
+
+        // SymptomService.submitSymptomFrequencies(statements)
+        //     .then((response) => {
+        //         console.log(response);
+        //         setStatements(response.statements);
+        //         setStatementsLoaded(true);
+
+        //     })
+    }
+
     return (
-        <div style={{ margin: '50px 200px' }}>
-            <h1>Start new appointment</h1>
-            <div style={{ margin: '50px' }}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Height</Form.Label>
-                    <Form.Control type="number" placeholder="Enter height in cm" value={height} onChange={(event) => setHeight(event.target.value)} />
-                </Form.Group>
+        <>
+            {!statementsLoaded && <div style={{ margin: '50px 200px' }}>
+                <h1>Start new appointment</h1>
+                <div style={{ margin: '50px' }}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Height</Form.Label>
+                        <Form.Control type="number" placeholder="Enter height in cm" value={height} onChange={(event) => setHeight(event.target.value)} />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Weight</Form.Label>
-                    <Form.Control type="number" placeholder="Enter current weight in kg" value={weight} onChange={(event) => setWeight(event.target.value)} />
-                </Form.Group>
-            </div>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Weight</Form.Label>
+                        <Form.Control type="number" placeholder="Enter current weight in kg" value={weight} onChange={(event) => setWeight(event.target.value)} />
+                    </Form.Group>
+                </div>
 
 
-            <h3>In the last two weeks, I have experienced the following symptoms: </h3>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <td></td>
-                        <td>Never</td>
-                        <td>Sometimes</td>
-                        <td>Often</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {symptoms.map((s, index) =>
-                        <tr key={s.id}>
-                            <td>{s.description}</td>
-
-                            {frequencies.map(frequency =>
-                                <td key={frequency}>
-                                    <Form.Check
-                                        name={`radioButton${s.id}`}
-                                        type="radio"
-                                        defaultChecked={s.frequency === frequency}
-                                        onClick={(() => onFrequencyChange(index, frequency))}
-                                    />
-                                </td>)}
+                <h3>In the last two weeks, I have experienced the following symptoms: </h3>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <td></td>
+                            <td>Never</td>
+                            <td>Sometimes</td>
+                            <td>Often</td>
                         </tr>
-                    )}
-                </tbody>
-            </Table>
-            <div style={{ margin: '50px' }}>
-                <h3>Write a little bit about how you have been feeling recently</h3>
-                <textarea style={{ width: '80%', height: '300px' }} value={comment} onChange={(event) => setComment(event.target.value)}></textarea>
-            </div>
+                    </thead>
+                    <tbody>
+                        {symptoms.map((symptom, index) =>
+                            <tr key={symptom.id}>
+                                <td>{symptom.description}</td>
 
-            <button onClick={() => submitSymptoms()}>Submit</button>
-        </div >
+                                {frequencies.map(frequency =>
+                                    <td key={frequency}>
+                                        <Form.Check
+                                            name={`radioButton${symptom.id}`}
+                                            type="radio"
+                                            defaultChecked={symptom.frequency === frequency}
+                                            onClick={(() => onFrequencyChange(index, frequency))}
+                                        />
+                                    </td>)}
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+                <div style={{ margin: '50px' }}>
+                    <h3>Write a little bit about how you have been feeling recently</h3>
+                    <textarea style={{ width: '80%', height: '300px' }} value={comment} onChange={(event) => setComment(event.target.value)}></textarea>
+                </div>
+
+                <button onClick={() => submitSymptoms()}>Submit</button>
+            </div >}
+            {statementsLoaded &&
+                <div>
+                    <h1>Statements Quiz</h1>
+                    <h3>Please fill out how much you relate to the following statements: </h3>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <td></td>
+                                <td>Strongly disagree</td>
+                                <td>Disagree</td>
+                                <td>No answer</td>
+                                <td>Agree</td>
+                                <td>Strongly agree</td>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {statements.map((statement, index) =>
+                                <tr key={statement.statementId}>
+                                    <td>{statement.statement}</td>
+
+                                    {statementResponses.map(response =>
+                                        <td key={response}>
+                                            <Form.Check
+                                                name={`radioButton${statement.statementId}`}
+                                                type="radio"
+                                                defaultChecked={statement.response === response}
+                                                onClick={(() => onStatementResponseChange(index, response))}
+                                            />
+                                        </td>)}
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
+                    <button onClick={() => submitStatements()}>Submit</button>
+
+
+                </div>}
+        </>
+
     );
 };
 
